a2126c01ef919d88cabf26808bfe27eba3f4045f
moritzbe <moritzberthold@hotmail.com>
2015-08-17 15:54:49 +0200

    first progress in rundin software ruby rundin.rb! ! exit

--- rundin.rb
+++ rundin.rb
@@ -13,12 +13,27 @@ end
 class Processor
 	def initialize (list)
 		@list = list
+		@teams = Array.new
+	end
+
+	def remove_line
+		@list = @list.split("\n")[1..-1].join("\n")
 	end
 
 	def process_list
-		@list
-		binding.pry
+		remove_line
+		container = []
+		@list = @list.each_line do |line|
+			line.gsub! /\n/, ''
+			line.gsub! /\t/, ' '
+			container = line.split(' ')
+	     	@teams << container
+	     	binding.pry
+		end	
+		puts @list
+		puts @teams
 	end
+end
 
 
 list = Reader.new
