41ccb628fafe8ad21548d26063373c1b6a320ec3
moritzbe <moritzberthold@hotmail.com>
2015-08-17 13:19:31 +0200

    debugging needed. Does the programm overwrite the initial board?

--- sudoku.rb
+++ sudoku.rb
@@ -24,39 +24,40 @@ class Reader
 	          	@board << l.collect {|x| x.to_i}
     		end
     	end
-    	@board
+    	return @board
 	end
 
 end
 
 
 
+
+
 class Solver
-	def initialize board
-		@board = board
+	def initialize
 		@m = Array.new
 	end
 
-	def select i, j
-		puts @board[i][j]
-		@number = @board[i][j]
-		@i = i
-		@j = j
+	def select k, l, board
+		@board = board
+		@i = k
+		@j = l
+		@board[@i][@j]
+		@number = @board[@i][@j]
 	end
-	
+
 	def check_line
 		@board[@i].each { |item| @m << item }
 	end
 
 	def check_row
-		9.times { |i| @m << @board[i-1][@j] }
-		#puts @m
+		9.times { |k| @m << @board[k-1][@j] }
 	end
 
-	def select_box axis, a
+	def select_box axis
 		if axis <= 2
 			a = [0, 1, 2]
-		elsif axis >= 3 && axis <= 5
+		elsif axis.between?(3, 5)
 			a = [3, 4, 5]
 		elsif axis >= 5
 			a = [6, 7, 8]
@@ -65,59 +66,73 @@ class Solver
 	end
 
 	def check_box
-		vx = select_box(@i, vx)
-		vy = select_box(@j, vy)
+		vx = select_box(@i)
+		vy = select_box(@j)
 		vx.each do |horz_item|
 			vy.each do |vert_item|
 				@m << @board[horz_item][vert_item]
 			end
 		end
-		puts @m
 	end
  	
-
-
 	def check_fit
+		match = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 		if @number==0
 			check_line
 			check_row
 			check_box
+			#binding.pry
+			values = @m.grep(Integer).sort.uniq - [0]
+			if values.length == 8
+				#binding.pry
+				finding = match - values
+				@board[@i][@j] = finding[0]
+			end
+		end
+		return @board
+	end
 
+	def solve
+		sudoku = Reader.new
+		sudoku.get_sudoku
+		board = sudoku.load
+		myboard = board
+
+		20.times do |i| 
+			for k in 0..8
+		   		for l in 0..8
+		   			select(k, l, myboard)
+					myboard = check_fit
+				end
+			end
+		resume = Printer.new(myboard)
+		resume.result
 		end
 	end
 
 
-
 end
-
-
-
 
 
 class Printer
 	def initialize board
-		@board = board
+		@boards = board
 	end
 
 	def result 
 		9.times do |i|
-  		print @board[i] 
+  		print @boards[i] 
 		end
 	end
 end
 
 
-sudoku = Reader.new
-sudoku.get_sudoku
 
-solve = Solver.new(sudoku.load)
-solve.select(0,0)
-solve.check_fit
+sudoku_instance = Solver.new
+sudoku_instance.solve
 
 
-#binding.pry
-resume = Printer.new(sudoku.load)
-#resume.result
+
 
 
 
